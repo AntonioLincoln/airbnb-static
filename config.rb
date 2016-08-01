e36e94ae63eb7a46ff9be3291e63075ddb4daf02
Lincoln <antoniolincoln@live.com>
2016-08-01 16:51:52 -0300

    adding files - proxy

--- config.rb
+++ config.rb
@@ -31,6 +31,11 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+["seb", "romain", "romeu"].each do |name|
+  proxy "/about/#{name}.html", "/about/template.html", :locals => { :person_name => name }, :ignore => true
+end
+
+
 ###
 # Helpers
 ###
